d888c4fe0706c2910181464103dd99a86d31b97b
Andrey Andreev <aa_andreev@mail.ru>
2021-10-05 21:50:55 +0300

    change

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
         String holderPath = "C:/Users/Andrey/Desktop";
         File file = new File(holderPath);
         long start = System.currentTimeMillis();
-        System.out.println(getSizeFiles(file));
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+        // System.out.println(getSizeFiles(file));
         long duration = System.currentTimeMillis() - start;
         System.out.println(duration);
     }
+
     private static long getSizeFiles(File file) {
         File[] listFile = file.listFiles();
         long sizeFiles = 0;
